a463ca99a9cf58a21ad6d7576ed043a71f666a6e
rim0621 <jeremy9106@gmail.com>
2017-11-08 14:13:42 +0900

    2017-11-08

--- 11066_mergeFile.cpp
+++ 11066_mergeFile.cpp
@@ -1,34 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
-
-int input[505];
+int sum[505];
 int cache[505][505];
-int sum(int i,int j)
-{
-	int total=0;
-	for(;i<=j;i++)
-		total+=input[i];
-	return total;
-}
 int main()
 {
 	int numCase;
 	scanf("%d",&numCase);
 	for(int excute=0;excute<numCase;excute++)
 	{
-		memset(input,0,sizeof(input));
-		memset(cache,100000,sizeof(cache));
+		memset(cache,0,sizeof(cache));
+		memset(sum,0,sizeof(sum));
 		int n;
 		scanf("%d",&n);
+
 		for(int i=1;i<=n;i++)
 		{
-			scanf("%d",&input[i]);
+			scanf("%d",&sum[i]);
+			sum[i]+=sum[i-1];
 		}
-		for(int i=1;i<=n;i++){
-			for(int j=i;j<=n;j++){
-				cache[i][j]=10000;	
+		int j;
+		for(int diagonal=1;diagonal<=n-1;diagonal++)
+		{
+		 	for(int i=1;i<=n-diagonal;i++)
+		 	{
+				j=i+diagonal;
+				cache[i][j]=999999999;
 				if(i==j)
 				{
 					cache[i][j]=0;
@@ -36,19 +32,22 @@ int main()
 				}
 				for(int k=i;k<=j-1;k++)
 				{
-					cache[i][j]=min(cache[i][j],cache[i][k]+cache[k+1][j]+sum(i,j));
+					cache[i][j]=min(cache[i][j],cache[i][k]+cache[k+1][j]-sum[i-1]+sum[j]);
 				}
+				
 			}
 		}
+
+
 		printf("%d\n",cache[1][n]);
-		for(int i=1;i<=n;i++)
+/*		for(int i=1;i<=n;i++)
 		{
 			for(int j=1;j<=n;j++)
 			{
 				cout<<cache[i][j]<< " ";
 			}
 			cout<<endl;
-		}
+		}*/
 	}
 	return 0;
 }
