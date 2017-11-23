b5d11767527b438d8095106a4d6ceedf311f9afb
rim0621 <jeremy9106@gmail.com>
2017-11-23 23:48:53 +0900

    2017-11-23

---
+++ 11719_OutputAsIs2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    string w; 
+    int i=0;
+    while (i<100){
+        getline(cin, w);
+        cout<<w<<endl;
+	i++;
+    }
+	return 0;
+}
+
+
