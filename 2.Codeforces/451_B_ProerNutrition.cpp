215becc4a27659838c31b4bb44960b3f00472b96
rim0621 <jeremy9106@gmail.com>
2017-12-20 11:35:42 +0900

    2017-12-20

--- 2.Codeforces/451_B_ProerNutrition.cpp
+++ 2.Codeforces/451_B_ProerNutrition.cpp
@@ -4,19 +4,16 @@ using namespace std;
 int main()
 {
 	int n,a,b,x;
-	float y;
+	int y;
 	cin>> n;
 	cin>> a;
 	cin>> b;
-	for(x=0;x<=10000000;x++)
+	for(x=0;a*x<=n;x++)
 	{
-		y=(n-a*x)/b;
-		if(y<0)
+		y=(n-a*x)%b;
+		if(y==0)
 		{
-			continue;
-		}
-		if(a*x+b*y==n)
-		{
+			y=(n-a*x)/b;
 			cout<<"YES"<<endl;
 			cout<<x<<" "<<y<<endl;	
 			return 0;
