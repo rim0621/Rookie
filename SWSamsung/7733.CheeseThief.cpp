e83df3bcd6b56ab612361f9f9c1f8fec5b2e83c9
rim0621 <jeremy9106@gmail.com>
2019-12-10 23:12:47 +0900

    2019-12-10

--- SWSamsung/7733.CheeseThief.cpp
+++ SWSamsung/7733.CheeseThief.cpp
@@ -7,18 +7,18 @@
 
 #include<bits/stdc++.h>
 
-#define MAX_DAY 100
-
+#define SIZE 102
 using namespace std;
-int cheese[103][103];
-int bitmap[103][103];
+int cheese[SIZE][SIZE];
+int bitmap[SIZE][SIZE];
 int dx[4]={0,-1,1,0};
 int dy[4]={1,0,0,-1};
-int days[101];
+int days[102];
 int n;
+int max_day;
 int get_day(int day)
 {
-	for(int i=day;i<=MAX_DAY;i++)
+	for(int i=1;i<=max_day;i++)
 	{
 		if(days[i]==1)
 		{
@@ -34,7 +34,7 @@ int find_chunk(int x, int y,char str)
 	{
 		return 0;
 	}
-	if(x > n || x < 1 || y > n || y < 1 || bitmap[x][y] == str)
+	if(x >= n+1 || x <= 0 || y >= n+1 || y <= 0 || bitmap[x][y] == str)
 	{
 		return 0;
 	}
@@ -52,7 +52,6 @@ int main(int argc, char** argv)
 	int test_case;
 	int T;
 	cin>>T;
-
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
 		cin >> n;
@@ -61,12 +60,14 @@ int main(int argc, char** argv)
 		int count=0;
 		int x=0;
 		int y=0;
+		max_day=0;
 		for(int i=1;i<=n;i++)
 		{
 			for(int j=1;j<=n;j++)
 			{
 				cin >> cheese[i][j];
 				days[cheese[i][j]]=1;
+				max_day=max(max_day,cheese[i][j]);
 			}
 		}
 		while(1){
@@ -81,32 +82,33 @@ int main(int argc, char** argv)
 					}
 				}
 			}
-			memcpy(bitmap,cheese,sizeof(int)*101*101);
+			memcpy(bitmap,cheese,sizeof(int)*SIZE*SIZE);
 			count=0;
 			cout<<"day : "<<day<<endl;
-			   for(int i=1;i<=n;i++)
-			   {
-			   for(int j=1;j<=n;j++)
-			   {
-			   cout << bitmap[i][j] <<"\t";
-			   }
-			   cout<<endl;
-			   }
-			 
-			if(day==-1)
+			for(int i=1;i<=n;i++)
+			{
+				for(int j=1;j<=n;j++)
+				{
+					cout << bitmap[i][j] <<"\t";
+				}
+				cout<<endl;
+			}
+
+			if(day==max_day)
 			{
 				break;
 			}
-			
+
 			for(int x=1;x<=n;x++){
 				for(int y=1;y<=n;y++){
-						count+=find_chunk(x,y,'O');
+					count+=find_chunk(x,y,'O');
 				}
 			}
 			cout<<"cnt : "<<count<<endl;
 			cout<<"-------------------------------------------------"<<endl;
+
 			max_cnt=max(max_cnt,count);
-			
+
 		}
 		cout<<"#"<<test_case<<" "<<max_cnt<<endl;
 
