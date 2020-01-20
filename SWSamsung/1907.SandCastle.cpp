d21c9ea78755b3979fb42a32a13017d35b3b43e4
rim0621 <jeremy9106@gmail.com>
2020-01-21 01:07:48 +0900

    2020-01-21

--- SWSamsung/1907.SandCastle.cpp
+++ SWSamsung/1907.SandCastle.cpp
@@ -1,8 +1,8 @@
 /****************************************************************
-*	author	:	jeremy											*
-*	problem :	1907. SandCastle								*
-*	data	:	20/01/08										*
-*****************************************************************/
+ *	author	:	jeremy											*
+ *	problem :	1907. SandCastle								*
+ *	data	:	20/01/08										*
+ *****************************************************************/
 
 
 
@@ -37,27 +37,24 @@ void find_weak(int x,int y)
 
 	for(int i=0;i<8;i++)
 	{
-		for(int j=0;j<8;j++)
-		{
-			next_x=x+dx[i];
-			next_y=y+dy[j];
+		next_x=x+dx[i];
+		next_y=y+dy[i];
 
-			if(next_x<0 || next_y<0 || next_x>=max_x || next_y>=max_y) // overrange
-				continue;
-			else{
-				if(sand_map[next_x][next_y]=='.')
-					current_val-=1;
-			}
-			if (current_val==0)
-			{
-				p.x=x;
-				p.y=y;
-				p.val=sand_map[x][y];
-				q.push(p);
-				return ;
-			}
-
+		if(next_x<0 || next_y<0 || next_x>=max_x || next_y>=max_y) // overrange
+			continue;
+		else{
+			if(sand_map[next_x][next_y]==-1)
+				current_val-=1;
 		}
+		if (current_val==0)
+		{
+			p.x=x;
+			p.y=y;
+			p.val=sand_map[x][y];
+			q.push(p);
+			return ;
+		}
+
 	}
 
 	return ;
@@ -71,8 +68,10 @@ int main()
 	{
 		//input
 		int result=0;
-		cin >> max_x
+		cin >> max_x;
 		cin >> max_y;
+		Position c_p;
+
 		for(int x=0;x<max_x;x++)
 		{
 			for(int y=0;y<max_y;y++)
@@ -88,15 +87,41 @@ int main()
 				find_weak(x,y);
 			}
 		}
-
+		result+=1;
+		
 		//queue check
-		if(q.empty())
-			cout<<result<<endl;
-		else
-		{
-			//큐에서 뽑고 .처리후 다시 8방향 
-		}
+		while(!q.empty()){
 			
-	
+			c_p=q.front();
+			q.pop();
+			int x=c_p.x;
+			int y=c_p.y;
+			sand_map[x][y]=-1;
+			for(int i=0;i<8;i++){
+				int next_x=x+dx[i];
+				int next_y=y+dy[i];
+				if(next_x<0 || next_y<0 || next_x>=max_x || next_y>=max_y) // overrange
+					continue;
+				else
+					find_weak(next_x,next_y);
+			}
+			result+=1;
+
+			//search able delete
+			for(int x=0;x<max_x;x++)
+			{
+				for(int y=0;y<max_y;y++)
+				{
+					find_weak(x,y);
+				}
+			}
+
+
+
+		}
+
+		cout<<result<<endl;
+
 	}
+	return 0;
 }
